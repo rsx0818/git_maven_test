120c21b485d27115c10949ec3a62bd3325ea95ea
rsx <2939199926@qq.com>
2022-07-27 15:45:44 +0800

    first commit

---
+++ src/main/java/com/java123/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.java123.git;
+
+/**
+ * @author rsx
+ * @Classname GitTest
+ * @Description 
+ * @Date 2022/7/27 15:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
