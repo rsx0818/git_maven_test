6711110716a3eb27fe3f061eceed9fd8615a1eb3
rsx <2939199926@qq.com>
2022-07-28 12:59:11 +0800

    添加3

--- src/main/java/com/java123/git/GitTest.java
+++ src/main/java/com/java123/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hod-fix");
         System.out.println("master test");
     }
 }
