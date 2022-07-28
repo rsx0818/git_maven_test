54b9d20999eb8d112a201ebee2283fb1ca8ad06c
rsx <2939199926@qq.com>
2022-07-28 12:46:18 +0800

    添加2

--- src/main/java/com/java123/git/GitTest.java
+++ src/main/java/com/java123/git/GitTest.java
@@ -3,11 +3,12 @@ package com.java123.git;
 /**
  * @author rsx
  * @Classname GitTest
- * @Description 
+ * @Description
  * @Date 2022/7/27 15:40
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
